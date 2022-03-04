6cd3a9d2a28d16e5ae0bafa331d61a3f8571d78c
morhale67 <87977630+morhale67@users.noreply.github.com>
2022-03-04 11:46:41 +0200

    Delete load_data.py

--- load_data.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import load_digits
-
-def load_data():
-    """importing the dataset"""
-    digits = load_digits()
-    x_data = digits.data
-    y_data = digits.target
-    return x_data, y_data
